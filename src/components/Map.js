256b0ff07a2dee97de02c1b80f75503c12b8d3bc
Calvin Nguyen <me@calvinln.com>
2018-10-20 15:59:26 -0700

    Add markers dynamically.

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,19 +9,14 @@ import {
 const MyMapComponent = withScriptjs(
   withGoogleMap(props => (
     <GoogleMap
-      defaultZoom={18}
+      defaultZoom={12}
       defaultCenter={{ lat: 33.651155, lng: -117.744094 }}
+      center={props.center}
     >
-      if (props.isMarkerShown)
-      {
-        <>
-          <Marker position={{ lat: 33.651155, lng: -117.744094 }} />
-          <Marker position={{ lat: 33.652027, lng: -117.743497 }} />
-          <Marker position={{ lat: 33.650421, lng: -117.743804 }} />
-          <Marker position={{ lat: 33.650431, lng: -117.744997 }} />
-          <Marker position={{ lat: 33.651496, lng: -117.745015 }} />
-        </>
-      }
+      if (props.markers)
+      {props.markers.map((marker, index) => (
+        <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+      ))}
     </GoogleMap>
   ))
 );
@@ -30,6 +25,7 @@ class Map extends Component {
   render() {
     return (
       <MyMapComponent
+        {...this.props}
         isMarkerShown
         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyD4YaWAb_bndQH5G39TYBg1oXR5JkHHDuc"
         loadingElement={<div style={{ height: `100%` }} />}
