8de6f14b4b34c866f5dceb1900aefbe8afbf81b8
Calvin Nguyen <me@calvinln.com>
2018-10-20 15:34:43 -0700

    Set up FourSquare API.

---
+++ src/API/index-fourSQ.js
@@ -0,0 +1,11 @@
+class FourSquare {
+  static getURL(endpoint) {
+    return fetch(
+      'https://api.foursquare.com/v2/venues/' +
+        endpoint +
+        '?near=Irvine,CA&query=tacos&limit=5&client_id=T0SRNGVDIIWOI2SDWLQAPRNFKDZJJUXEG100I0A1ZK4QHDVP&client_secret=MVFMYTMUVWHISANRBL4NWUNBRONHXQDDYDGWHYMUWYZVMJNY&v=20181020'
+    ).then(resp => resp.json());
+  }
+}
+
+export default FourSquare;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map';
+import FourSquare from './API/index-fourSQ';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      venues: [],
+      markers: [],
+      center: []
+    };
+  }
+
+  componentDidMount() {
+    console.log(FourSquare.getURL('search'));
+    FourSquare.getURL('search').then(results => {
+      const venues = results.response.venues;
+      const markers = venues.map(venue => venue.location);
+      const center = results.response.geocode.feature.geometry.center;
+      this.setState({ venues, markers, center });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map {...this.state} />
       </div>
     );
   }
