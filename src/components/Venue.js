c67d464b45ce74d7bf8752b5f021df8b7d388faf
Calvin Nguyen <me@calvinln.com>
2018-10-22 16:24:45 -0700

    Add tab index for venues.

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Venue extends Component {
   render() {
     return (
-      <li className={'venue'} onClick={() => this.props.venueClick(this.props)}>
+      <li
+        tabIndex="0"
+        className={'venue'}
+        onClick={() => this.props.venueClick(this.props)}
+      >
         {this.props.name}
       </li>
     );
